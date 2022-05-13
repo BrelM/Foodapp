43cbdc3a9791f8bac1b50b0515f01e81e2ec93a9
Brel Mbe <brelmbe@gmail.com>
2022-05-13 20:51:07 +0100

    Updated classes models. Now, creating django forms

--- foodapp/visitor/models.py
+++ foodapp/visitor/models.py
@@ -20,12 +20,16 @@ MINERALS = [
 
 
 
-class Food(models.Model):
+class Ressource(models.Model):
     '''
-        Implementation of Food class
+        Implementation of Ressource class
+        Food and Meal classes inherit it.
     '''
-    name = models.CharField(max_length=30, null=False, default='unknown food')
-    unit = models.CharField(max_length=50, null=False, default=' unit(s) of ')
+
+    class Meta:
+        abstract = True
+
+    name = models.CharField(max_length=30, null=False, default='unknown ressource')
     fat = models.FloatField(default=0)
     proteins = models.FloatField(default=0)
     fiber = models.FloatField(default=0)
@@ -37,36 +41,75 @@ class Food(models.Model):
     #picture = models.ImageField()
 
 
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_fat(self):
+        return self.fat
+
+    def get_proteins(self):
+        return self.proteins
+
+    def get_fiber(self):
+        return self.fiber
+
+    def get_carbohydrates(self):
+        return self.carbohydrates
+
+    def get_water(self):
+        return self.water
+
+    def get_vitamins(self):
+        return self.vitamins
+
+    def get_minerals(self):
+        return self.minerals
+
+    def get_kcal(self):
+        return self.kcal
+
+
+
+class Food(Ressource):
+    '''
+        Implementation of Food class
+    '''
+    unit = models.CharField(max_length=50, null=False, default=' unit(s) of ')
 
 
 
 
-
-class Meal(models.Model):
+class Meal(Ressource):
     '''
         Implementation of Meal class
         A meal might be composed of Food objects but also of Meals objects
-    '''
-    name = models.CharField(max_length=30, null=False, default='unknown food')
-    recipe = models.CharField(max_length=2000, null=False, default='Preparation:\ncooking phase:\n')
-    fat = models.FloatField(default=0)
-    proteins = models.FloatField(default=0)
-    fiber = models.FloatField(default=0)
-    carbohydrates = models.FloatField(default=0)
-    water = models.FloatField(default=0)
-    vitamins = models.CharField(choices=VITAMINS, max_length=3)
-    minerals = models.CharField(choices=MINERALS, max_length=20)
-    kcal = models.FloatField(default=0)
-    #picture = models.ImageField()
+    '''    
 
-    ingredients = models.ManyToManyField(Food, related_name='+', blank=True)
+    ingredients = models.ManyToManyField(Food, related_name='+', blank=True, through='Uses')
     submeals = models.ManyToManyField('self', blank=True)
     operator = models.ForeignKey(Operator, related_name='meals', blank=True, null=True, on_delete=models.CASCADE)
     likers = models.ManyToManyField(Operator, related_name='likedMeals', through='MealAppreciation')
     commentors = models.ManyToManyField(Operator, related_name='commentedMeals', through='MealCommenting')
 
+    def update_info(self):
+        '''
+            Update nutrient attributes values taking in charge the nutrient attributes values of the composing Food objects
+        '''
 
+        elements = list(self.elements.all()) + list(self.submeals.all())
 
+        for element in elements:
+            self.fat += element.fat
+            self.proteins += element.proteins
+            self.fiber += element.fiber
+            self.carbohydrates += element.carbohydrates
+            self.water += element.water
+            self.kcal += element.kcal
+            self.vitamins += element.vitamins
+            self.minerals += element.minerals
 
 
 
@@ -133,6 +176,13 @@ class MenuCommenting(models.Model):
 
 
 
+class Uses(models.Model):
+    '''
+        Implementation of the commenting many to many relationship between Operator model and Menu model
+    '''
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
 
 
 
